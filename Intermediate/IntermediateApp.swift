6ef23a9e894d9f2a6ff0f0b43acfa41c39ae9f93
Amir Diafi <59441376+AmirDiafi@users.noreply.github.com>
2022-07-15 10:56:26 +0100

    Initial Commit

---
+++ Intermediate/IntermediateApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntermediateApp.swift
+//  Intermediate
+//
+//  Created by AmirDiafi on 7/15/22.
+//
+
+import SwiftUI
+
+@main
+struct IntermediateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
